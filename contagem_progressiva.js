6f2a1575b5fff8d99805d8867d1cd7f5826e6602
Paulo <paulo.ipa9@gmail.com>
2024-01-18 15:51:36 -0300

    Desafio: hora da prática Alura

---
+++ contagem_progressiva.js
@@ -0,0 +1,8 @@
+let numeroMaximo = prompt('digite um número maior que 0');
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador = contador + 1;
+    
+}
---
+++ contagem_regressiva.js
@@ -0,0 +1,7 @@
+let numeroInicial = prompt('Digite um número para regredir no console.')
+
+while (numeroInicial >= 0) {
+    console.log(numeroInicial);
+    numeroInicial = numeroInicial - 1;
+    
+}
